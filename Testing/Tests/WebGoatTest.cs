8d8083c78d9fe75b527db2133f075d01c8f7b8d5
Adam Vlasak <adam.vlasak@gmail.com>
2020-03-14 23:48:09 +0100

    use testcase attribute

--- Testing/Tests/WebGoatTest.cs
+++ Testing/Tests/WebGoatTest.cs
@@ -7,21 +7,28 @@ namespace Testing.Tests
     public class WebGoatTest : BaseTest
     {
         [Test]
-        public void LoginUnsuccessfulTest()
+        [TestCase("guest", "Guest")]
+        [TestCase("admin", "foobar")]
+        [TestCase("webgoat", "Webgoat")]
+        public void LoginUnsuccessfulTest(string username, string password)
         {
             var p = new WebGoatLoginPage(driver);
-            p.Login("guest", "foobar");
+            p.Login(username, password);
             Assert.That(p.ErrorMessage.Displayed, Is.True);
             Assert.That(p.ErrorMessage.Text, Is.EqualTo("Invalid username and password!"));
+            p.Screenshot();
         }
 
         [Test]
-        public void LoginSuccessfulTest()
+        [TestCase("guest", "guest")]
+        [TestCase("webgoat", "webgoat")]
+        public void LoginSuccessfulTest(string username, string password)
         {
             var p = new WebGoatLoginPage(driver);
-            p.Login("guest", "guest");
+            p.Login(username, password);
             Assert.That(p.LessonTitle.Displayed, Is.True);
             Assert.That(p.LessonTitle.Text, Is.EqualTo("How to work with WebGoat"));
+            p.Screenshot();
         }
     }
 }
