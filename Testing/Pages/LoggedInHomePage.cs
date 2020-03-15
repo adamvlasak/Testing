79bdcd53165db092d6cf675021e0ce4b27e3455a
Adam Vlasak <adam.vlasak@gmail.com>
2020-03-15 01:42:39 +0100

    more tests

--- Testing/Pages/LoggedInHomePage.cs
+++ Testing/Pages/LoggedInHomePage.cs
@@ -1,12 +1,18 @@
 ﻿using OpenQA.Selenium;
+using System;
 
 namespace Testing.Pages
 {
-    public class LoggedInHomePage : BasePage
+    public sealed class LoggedInHomePage : BasePage
     {
         public IWebElement LessonTitle => _wait.Until(driver => driver.FindElement(By.CssSelector("h1#lesson-title")));
         public IWebElement UserMenu => _wait.Until(driver => driver.FindElement(By.CssSelector("button#user-menu")));
         public IWebElement LogoutLink => _wait.Until(driver => driver.FindElement(By.LinkText("Logout")));
+        public IWebElement ShowDeveloperOptionsLink => _wait.Until(driver => driver.FindElement(By.LinkText("Show developer controls")));
+        public IWebElement DeveloperControlContainer => _wait.Until(driver => driver.FindElement(By.CssSelector("div#developer-control-container")));
+        public IWebElement EnableLabelDebuggingLink => _wait.Until(driver => driver.FindElement(By.LinkText("Enable label debugging")));
+        public IWebElement DisableabelDebuggingLink => _wait.Until(driver => driver.FindElement(By.LinkText("Disable label debugging")));
+        public IWebElement LessonProgressStatus => _wait.Until(driver => driver.FindElement(By.CssSelector("div#lesson-progress")));
 
         public LoggedInHomePage(IWebDriver driver) : base(driver)
         {
@@ -17,6 +23,13 @@ namespace Testing.Pages
             UserMenu.Click();
             LogoutLink.Click();
             return new WebGoatLoginPage(_driver);
+        }
+
+        public void EnableLabelDebugging()
+        {
+            UserMenu.Click();
+            ShowDeveloperOptionsLink.Click();
+            EnableLabelDebuggingLink.Click();
         }
     }
 }
--- Testing/Tests/WebGoatTest.cs
+++ Testing/Tests/WebGoatTest.cs
@@ -47,11 +47,29 @@ namespace Testing.Tests
             Assert.That(driver.Url, Is.EqualTo("http://webapp:8080/WebGoat/login.mvc"));
             var lp = p.Login(username, password);
             Assert.That(lp.LessonTitle.Displayed, Is.True);
+            Assert.That(lp.LessonProgressStatus.Text, Is.EqualTo("Congratulations. You have successfully completed this lesson."));
             p = lp.Logout();
             Assert.That(p.AlertSuccess.Displayed, Is.True);
+            Assert.That(p.AlertSuccess.Text, Is.EqualTo("You have logged out successfully"));
             Assert.That(p.LoginLink.Displayed, Is.True);
             Assert.That(driver.Url, Is.EqualTo("http://webapp:8080/WebGoat/logout.mvc"));
             p.Screenshot();
+        }
+
+        [Test]
+        [TestCase("guest", "guest")]
+        [TestCase("webgoat", "webgoat")]
+        public void CompleteLesson(string username, string password)
+        {
+            var p = new WebGoatLoginPage(driver);
+            p.Visit();
+            Assert.That(driver.Url, Is.EqualTo("http://webapp:8080/WebGoat/login.mvc"));
+            var lp = p.Login(username, password);
+            Assert.That(lp.LessonTitle.Displayed, Is.True);
+            lp.EnableLabelDebugging();
+            Assert.That(lp.LessonProgressStatus.Text, Is.EqualTo("Congratulations. You have successfully completed this lesson."));
+            Assert.That(lp.LessonProgressStatus.Displayed, Is.True);
+            lp.Screenshot();
         }
     }
 }
