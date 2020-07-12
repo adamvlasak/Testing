ad0a6d78549f8a900115e85b5e987860967570c1
Adam Vlasak <adam.vlasak@gmail.com>
2020-07-12 22:25:00 +0200

    one more example using wait in props

---
+++ Testing/Pages/BigWebSitePageSync.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+
+namespace Testing.Pages
+{
+    public class BigWebsitePageSync : BasePage
+    {
+        public IWebElement Title => Wait.Until(driver => driver.FindElement(By.CssSelector(".logo--header")));
+        public IWebElement App => Wait.Until(driver => driver.FindElement(By.CssSelector("div#app-root")));
+
+        public BigWebsitePageSync(System.Uri baseUrl, IWebDriver driver) : base(baseUrl, driver)
+        {
+        }
+
+        public BigWebsitePageSync(IWebDriver driver) : base(driver)
+        {
+        }
+    }
+}
--- Testing/Tests/BigWebSiteTest.cs
+++ Testing/Tests/BigWebSiteTest.cs
@@ -44,5 +44,17 @@ namespace Testing.Tests
             Assert.That(p.Title.Displayed, Is.True, $"Title not found on the web page {WebDriver.Url}");
             Assert.That(p.App.Displayed, Is.True, "No app found on the web page");
         }
+
+        [Test]
+        public void WaitForReady_Using_WaitInProps()
+        {
+            var p = new BigWebsitePage(new System.Uri("https://wired.com"), WebDriver);
+            p.Visit();
+            Assert.That(p.Title.Displayed, Is.True, $"Title not found on the web page {WebDriver.Url}");
+            Assert.That(p.App.Displayed, Is.True, "No app found on the web page");
+            WebDriver.Navigate().Refresh();
+            Assert.That(p.Title.Displayed, Is.True, $"Title not found on the web page {WebDriver.Url}");
+            Assert.That(p.App.Displayed, Is.True, "No app found on the web page");
+        }
     }
 }
