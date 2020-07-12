9ac80c74e48bb795896aa858b4ab535927ece459
Adam Vlasak <adam.vlasak@gmail.com>
2020-07-12 21:01:48 +0200

    we don't need waits inside pages if we use waitforready

--- Testing/Pages/BigWebSitePage.cs
+++ Testing/Pages/BigWebSitePage.cs
@@ -4,8 +4,8 @@ namespace Testing.Pages
 {
     public class BigWebsitePage : BasePage
     {
-        public IWebElement Title => Wait.Until(driver => driver.FindElement(By.CssSelector(".logo--header")));
-        public IWebElement Paragraph => Wait.Until(driver => driver.FindElement(By.CssSelector("p")));
+        public IWebElement Title => WebDriver.FindElement(By.CssSelector(".logo--header"));
+        public IWebElement Paragraph => WebDriver.FindElement(By.CssSelector("p"));
 
         public BigWebsitePage(System.Uri baseUrl, IWebDriver driver) : base(baseUrl, driver)
         {
