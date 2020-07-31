d37d8a43b01f158943d4c9d1594b327b20617e91
Adam Vlasak <adam.vlasak@gmail.com>
2020-08-01 00:22:39 +0200

    fix test

--- Testing/Tests/BigWebSiteTest.cs
+++ Testing/Tests/BigWebSiteTest.cs
@@ -48,7 +48,7 @@ namespace Testing.Tests
         [Test]
         public void WaitForReady_Using_WaitInProps()
         {
-            var p = new BigWebsitePage(new System.Uri("https://wired.com"), WebDriver);
+            var p = new BigWebsitePageSync(new System.Uri("https://wired.com"), WebDriver);
             p.Visit();
             Assert.That(p.Title.Displayed, Is.True, $"Title not found on the web page {WebDriver.Url}");
             Assert.That(p.App.Displayed, Is.True, "No app found on the web page");
