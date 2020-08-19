83b31021c69a4bd3cb32d77eba975fe864b7ee92
Adam Vlasak <adam.vlasak@gmail.com>
2020-08-19 02:04:23 +0200

    computed prop

--- WebGoat/Data/Link.cs
+++ WebGoat/Data/Link.cs
@@ -6,7 +6,7 @@ namespace WebGoat.Data
     public class Link
     {
         public string Title { get; set; }
-        public string Path { get; set; }
+        public string Path => $"/start.mvc#attack/{Id}/{Score}";
         public Uri Domain => new Uri($"{Configuration.ApplicationUrl}");
         public Uri Url => new Uri($"{Domain}{Path}");
         public string Parent { get; set; }
