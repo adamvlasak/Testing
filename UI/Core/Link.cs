192f35b336351e21b92aac75fc63e5cb631bb812
Adam Vlasak <adam.vlasak@gmail.com>
2020-09-03 21:08:47 +0200

    rename domain to host in Link.cs

--- UI/Core/Link.cs
+++ UI/Core/Link.cs
@@ -7,8 +7,8 @@ namespace UI.Core
     {
         public string Title { get; set; }
         public string Path => $"/start.mvc#attack/{Id}/{Score}";
-        public Uri Domain => new Uri($"{Configuration.ApplicationUrl}");
-        public Uri Url => new Uri($"{Domain}{Path}");
+        public Uri Host => new Uri($"{Configuration.ApplicationUrl}");
+        public Uri Url => new Uri($"{Host}{Path}");
         public string Parent { get; set; }
         public int Score { get; set; }
         public int Id { get; set; }
